2cd17b059c0a882bab3f805933762e420dff5c79
Vic <victorsuero98@gmail.com>
2025-11-15 21:13:22 +0100

    create db connection and test wrinting some entities

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,26 @@
 import express from 'express';
+import { MySQLDataSource } from './config/MySQL-datasource';
+
+
+(async () => {
+    try {
+        await MySQLDataSource.initialize();
+        console.log('Data Source has been initialized!');
+
+        // const products = MySQLDataSource.getRepository(Product);
+        // const category = MySQLDataSource.getRepository(Category);
+        // let cat = await category.findOne({ where: { id: 1 } });
+        // let cat = await category.findOneBy({ id: 1 });
+        // console.log(cat);
+        // let pr = new Product();
+        // pr.category = cat!;
+        // pr.name = 'TV 85 pulgadas';
+        // products.insert(pr);
+    } catch (error) {
+        console.error('Error during Data Source initialization', error);
+    }
+})();
+
 const app = express();
 const port = 3000;
 
---
+++ backend/src/config/MySQL-datasource.ts
@@ -0,0 +1,17 @@
+import { DataSource } from 'typeorm';
+import { envs } from './envs';
+
+export const MySQLDataSource = new DataSource({
+    type: 'mysql',
+    host: envs.DATABASE_HOST!,
+    port: envs.DATABASE_PORT!,
+    username: envs.DATABASE_USER!,
+    password: envs.DATABASE_PASSWORD!,
+    database: envs.DATABASE_NAME!,
+    entities: ['src/Models/**/*.{js,ts}'],
+    // synchronize: true,
+    logging: true,
+    extra: {
+        connectionLimit: 5,
+    },
+});
