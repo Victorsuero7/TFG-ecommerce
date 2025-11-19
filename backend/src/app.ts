712dd963686ea1f302288280a9f22f91c82958e8
Vic <victorsuero98@gmail.com>
2025-11-19 20:47:01 +0100

    db created and conected succesfully

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,20 +2,10 @@ import express from 'express';
 import { MySQLDataSource } from './config/MySQL-datasource';
 
 
-(async () => {
+(() => {
     try {
-        await MySQLDataSource.initialize();
+        MySQLDataSource.initialize();
         console.log('Data Source has been initialized!');
-
-        // const products = MySQLDataSource.getRepository(Product);
-        // const category = MySQLDataSource.getRepository(Category);
-        // let cat = await category.findOne({ where: { id: 1 } });
-        // let cat = await category.findOneBy({ id: 1 });
-        // console.log(cat);
-        // let pr = new Product();
-        // pr.category = cat!;
-        // pr.name = 'TV 85 pulgadas';
-        // products.insert(pr);
     } catch (error) {
         console.error('Error during Data Source initialization', error);
     }
