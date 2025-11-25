2756f7e70dffa69bdb77541b6a4bb44227b46104
Sofia Fernández <sofiaffeijoo98@gmail.com>
2025-11-25 17:43:41 +0100

    se cambia el nav para el app component para que aparezca en todas las paginas y se aplica el responsive

--- ecommerce-app-front/src/app/pages/dashboard/dashboard.component.ts
+++ ecommerce-app-front/src/app/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavComponent } from '../../components/nav/nav.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [NavComponent, CommonModule],
+  imports: [ CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
