9c632d5eb93c8368d4fb5ecfa479b0c7071ecac1
Vic <victorsuero98@gmail.com>
2025-11-27 20:07:53 +0100

    create repoitories right way

---
+++ backend/src/repositories/TypeORMRepository.ts
@@ -0,0 +1,32 @@
+import { Repository, DataSource, EntityTarget, ObjectLiteral, FindOptionsWhere } from "typeorm";
+
+export abstract class TypeORMRepository<T extends ObjectLiteral, ID> {
+    protected repo: Repository<T>;
+
+    constructor(
+        entity: EntityTarget<T>,
+        protected readonly datasource: DataSource
+    ) {
+        this.repo = datasource.getRepository(entity);
+    }
+
+    async findAll(): Promise<T[]> {
+        return await this.repo.find();
+    }
+
+    async findOneById(id: ID): Promise<T | null> {
+        return await this.repo.findOneBy({ id } as FindOptionsWhere<T>);
+    }
+
+    async save(entity: T): Promise<T> {
+        return await this.repo.save(entity);
+    }
+
+    async remove(entity: T): Promise<T> {
+        return await this.repo.remove(entity);
+    }
+
+    async findBy(conditions: Partial<T>): Promise<T[]> {
+        return await this.repo.find({ where: conditions });
+    }
+}
