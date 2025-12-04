85e093a9000f3860135ed11994e4b0cf6431a7e8
Vic <victorsuero98@gmail.com>
2025-12-04 22:49:13 +0100

    fix method names

--- backend/src/services/CategoryService.ts
+++ backend/src/services/CategoryService.ts
@@ -1,8 +1,8 @@
 import { Category } from "../Models/category.entity";
 
 export interface CategoryService {
-    gettAll(): Promise<Category[]>
-    getOne(id: number): Promise<Category | null>
+    getAll(): Promise<Category[]>
+    getById(id: number): Promise<Category | null>
     insert(category: Category): Promise<Category>
     findByName(name: string): Promise<Category | null>
     findByDescription(description: string): Promise<Category | null>
