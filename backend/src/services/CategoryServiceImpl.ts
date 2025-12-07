71700042b226f4eca0e44ddcb410dec7336c9bd8
Vic <116363188+Victorsuero7@users.noreply.github.com>
2025-12-07 19:33:54 +0100

    Feature/category service (#19)
    * Add: ProductService and ProductServiceImpl created
    
    * Add: CategoryService and CategoryServiceImpl created
    
    * fix method names
    
    ---------
    
    Co-authored-by: Enrique Maestre <enrique.maestre265@comunidadunir.net>

--- backend/src/services/CategoryServiceImpl.ts
+++ backend/src/services/CategoryServiceImpl.ts
@@ -2,21 +2,21 @@ import { Category } from "../Models/category.entity";
 import { CategoryRepository } from "../repositories/CategoryRepository";
 import { CategoryService } from "./CategoryService";
 
-export class CategoryServiceImpl implements CategoryService{
+export class CategoryServiceImpl implements CategoryService {
     private readonly repo: CategoryRepository;
-    constructor(repo: CategoryRepository){
+    constructor(repo: CategoryRepository) {
         this.repo = repo;
     }
-    async gettAll(): Promise<Category[]> {
+    async getAll(): Promise<Category[]> {
         return await this.repo.findAll()
     }
-    async getOne(id: number): Promise<Category | null> {
+    async getById(id: number): Promise<Category | null> {
         return await this.repo.findOneById(id)
     }
     async insert(category: Category): Promise<Category> {
         return await this.repo.save(category)
     }
-    async findByName(name: string): Promise<Category | null>{
+    async findByName(name: string): Promise<Category | null> {
         return await this.repo.findByName(name)
     }
     async findByDescription(description: string): Promise<Category | null> {
