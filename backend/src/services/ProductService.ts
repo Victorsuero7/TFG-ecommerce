78eee423f05fd2ba1b6e0619ad9ba4fbce85b911
Vic <116363188+Victorsuero7@users.noreply.github.com>
2025-12-07 19:31:56 +0100

    Feature/product service (#20)
    * Add: ProductService and ProductServiceImpl created
    
    * fix method names
    
    ---------
    
    Co-authored-by: Enrique Maestre <enrique.maestre265@comunidadunir.net>

--- backend/src/services/ProductService.ts
+++ backend/src/services/ProductService.ts
@@ -2,6 +2,6 @@ import { Product } from "../Models/product.entity";
 
 export interface ProductService {
     getAll(): Promise<Product[]>
-    getOne(id: number): Promise<Product | null>
+    getById(id: number): Promise<Product | null>
     insert(product: Product): Promise<Product>
 }
