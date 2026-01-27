c586eaec373227eb7818508096bbfe505b0a1b48
Victor Suero <victorsuero98@gmail.com>
2026-01-27 16:16:05 +0100

    create standar user dto

--- backend/src/dtos/UserDTO.ts
+++ backend/src/dtos/UserDTO.ts
@@ -1,22 +1,19 @@
 import { User, ROLE } from "../Models/user.entity";
 
 export class UserDTO {
-    name!: string
-    lastName!: string
-    email!: string
-    phoneNumber!: string
-    // birthDate!: Date;
-    role!: ROLE | null
-
-    private constructor() { }
+    constructor(name: string,
+        lastName: string,
+        email: string,
+        phoneNumber: string,
+        // birthDate!: Date;
+        role: ROLE | null) { }
 
     static fromEntity(user: User): UserDTO {
-        const dto = new UserDTO();
-        dto.name = user.name;
-        dto.lastName = user.lastName;
-        dto.email = user.email;
-        dto.phoneNumber = user.phoneNumber;
-        dto.role = user.role;
-        return dto;
+        return new UserDTO(user.name, user.lastName, user.email, user.phoneNumber, user.role)
+    }
+
+    static createDTO(object: { [key: string]: any; }): UserDTO {
+        const { name, lastName, email, phoneNumber, role } = object;
+        return new UserDTO(name, lastName, email, phoneNumber, role)
     }
 }
