0d07d18b2f6a89e3ad44d16f5f7a690f540edc01
Victor Suero <victorsuero98@gmail.com>
2026-02-09 12:38:39 +0100

    fix types

--- backend/src/utils/AuthorizationMiddleware.ts
+++ backend/src/utils/AuthorizationMiddleware.ts
@@ -1,12 +1,14 @@
 import { Request, Response, NextFunction } from "express"
 import { JWTAdapter } from "./Jwt"
 import { JWTSchema } from "./JWTSchema"
+import { ROLE } from "../Models/user.entity"
 
-export function authorizationMiddleware(acceptedRoles: string[]) {
+export function authorizationMiddleware(acceptedRoles: ROLE[]) {
     async (req: Request, res: Response, next: NextFunction) => {
         const token = req.headers.authorization?.split(' ')[1]
-        const payload = await JWTAdapter.validateToken<JWTSchema>(token!)
+        if (!token) return res.status(401).json({ message: "Unauthorized" })
 
+        const payload = await JWTAdapter.validateToken<JWTSchema>(token!)
         if (!payload) return res.status(401).json({ message: "Unauthorized" })
         if (!acceptedRoles.includes(payload.role)) return res.status(403).json({ message: "Forbidden" })
 
