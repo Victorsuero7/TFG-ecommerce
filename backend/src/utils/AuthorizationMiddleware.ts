7b8c47e8277e9702bfb471f01b7112eacbe61757
Vic <victorsuero98@gmail.com>
2026-02-14 15:56:03 +0100

    fixed and organiced and divided responsabilities

--- backend/src/utils/AuthorizationMiddleware.ts
+++ backend/src/utils/AuthorizationMiddleware.ts
@@ -3,15 +3,35 @@ import { JWTAdapter } from "./Jwt"
 import { JWTSchema } from "./JWTSchema"
 import { ROLE } from "../Models/user.entity"
 
-export function authorizationMiddleware(acceptedRoles: ROLE[]) {
-    async (req: Request, res: Response, next: NextFunction) => {
-        const token = req.headers.authorization?.split(' ')[1]
-        if (!token) return res.status(401).json({ message: "Unauthorized" })
+// export function authorizationMiddleware(acceptedRoles: ROLE[]) {
+//     async (req: Request, res: Response, next: NextFunction) => {
+//         const token = req.headers.authorization?.split(' ')[1]
+//         if (!token) return res.status(401).json({ message: "Unauthorized" })
 
-        const payload = await JWTAdapter.validateToken<JWTSchema>(token!)
-        if (!payload) return res.status(401).json({ message: "Unauthorized" })
-        if (!acceptedRoles.includes(payload.role)) return res.status(403).json({ message: "Forbidden" })
+//         const payload = await JWTAdapter.validateToken<JWTSchema>(token!)
+//         if (!payload) return res.status(401).json({ message: "Unauthorized" })
+//         if (!acceptedRoles.includes(payload.role)) return res.status(403).json({ message: "Forbidden" })
 
-        next()
+//         next()
+//     }
+// }
+
+export class RBACMiddleware {
+    static requireAutentication() {
+        async (req: Request, res: Response, next: NextFunction) => {
+            const token = req.headers.authorization?.split(' ')[1]
+            if (!token) return res.status(401).json({ message: "Unauthorized" })
+            const payload = await JWTAdapter.validateToken<JWTSchema>(token!)
+            if (!payload) return res.status(401).json({ message: "Unauthorized" })
+            req.user = payload
+            next()
+        }
+    }
+
+    static requireRole(acceptedRoles: ROLE[]) {
+        async (req: Request, res: Response, next: NextFunction) => {
+            if (!acceptedRoles.includes(req.user.role)) return res.status(403).json({ message: "Forbidden" })
+            next()
+        }
     }
 }
