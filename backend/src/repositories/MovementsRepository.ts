618e667705684926459dfda98c4c55df2a2a8ec3
Vic <victorsuero98@gmail.com>
2026-02-19 19:42:46 +0100

    imporove repository filter options

--- backend/src/repositories/MovementsRepository.ts
+++ backend/src/repositories/MovementsRepository.ts
@@ -1,13 +1,34 @@
 import { Between, DataSource, EntityManager, Or } from "typeorm";
 import { TypeORMRepository } from "./TypeORMRepository";
 import { Movement } from "../Models/DataMovements.entity";
+import { User } from "../Models/user.entity";
+import { Product } from "../Models/product.entity";
 
+type Conditions = {
+    userId?: number;
+    productId?: number;
+};
 export class MovementRepository extends TypeORMRepository<Movement, number> {
     constructor(datasource: DataSource) {
         super(Movement, datasource)
     }
 
     private RELATIONS = ["category", 'modifiedBy']
+
+    private buildWhere(conditions: Conditions) {
+        const where: Partial<Movement>[] = [];
+        if (conditions.userId !== undefined) {
+            where.push({
+                modifiedBy: { id: conditions.userId } as User
+            });
+        }
+        if (conditions.productId !== undefined) {
+            where.push({
+                product: { id: conditions.productId } as Product
+            });
+        }
+        return where;
+    }
 
     async transaction(cb: (entityManager: EntityManager) => Promise<unknown>) {
         return this.datasource.transaction(cb)
@@ -30,6 +51,18 @@ export class MovementRepository extends TypeORMRepository<Movement, number> {
         })
     }
 
+    async findByConditions(conditions: Conditions, offset: number, limit: number, relations: string[] = this.RELATIONS): Promise<[Movement[], number]> {
+        return await this.repo.findAndCount({
+            where: this.buildWhere(conditions),
+            order: {
+                id: "ASC"
+            },
+            skip: offset,
+            take: limit,
+            relations: relations
+        })
+    }
+
     async findByUser(userId: number, offset: number, limit: number, relations: string[] = this.RELATIONS): Promise<[Movement[], number]> {
         return await this.repo.findAndCount({
             where: { modifiedBy: { id: userId } },
@@ -45,7 +78,7 @@ export class MovementRepository extends TypeORMRepository<Movement, number> {
     async findByProduct(productId: number, offset: number, limit: number, relations: string[] = this.RELATIONS): Promise<[Movement[], number]> {
         return await this.repo.findAndCount({
             // where: { product: {id:productId} },
-            where:{ },
+            where: {},
             order: {
                 id: "ASC"
             },
