48a535ee3492e4dc33aee06257a5ea9bb6689401
sofiaffernandez <sofiaffeijoo98@gmail.com>
2026-02-23 17:15:32 +0100

    softdelete

--- backend/src/repositories/TypeORMRepository.ts
+++ backend/src/repositories/TypeORMRepository.ts
@@ -15,9 +15,9 @@ export abstract class TypeORMRepository<T extends ObjectLiteral, ID> {
         return await this.repo.find();
     }
 
-    // async findAllByPage(offset: number, limit: number): Promise<T[]> {
-    //     return await this.repo.find({ skip: offset, take: limit })
-    // }
+    async findAllByPage(offset: number, limit: number): Promise<T[] | [T[], number]> {
+        return await this.repo.find({ skip: offset, take: limit })
+    }
 
     async findOneById(id: ID): Promise<T | null> {
         return await this.repo.findOneBy({ id } as FindOptionsWhere<T>);
