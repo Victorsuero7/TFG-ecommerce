99eab5938d39d817435a4323eb58602fef853ebb
sofiaffernandez <sofiaffeijoo98@gmail.com>
2026-03-14 14:44:45 +0100

    modo noche

--- ecommerce-app-front/src/app/pages/user-register/user-register.component.ts
+++ ecommerce-app-front/src/app/pages/user-register/user-register.component.ts
@@ -82,7 +82,7 @@ export class UserRegisterComponent {
     const cp = cpControl.value;
 
     if (!cp) {
-      if (cpControl.hasError('Las contraseñas no coinciden')) {
+      if (cpControl.hasError('mismatch')) {
         const errors = { ...cpControl.errors };
         delete (errors as any).mismatch;
         if (Object.keys(errors).length === 0) cpControl.setErrors(null);
@@ -94,7 +94,7 @@ export class UserRegisterComponent {
     if (pwd !== cp) {
       cpControl.setErrors({ ...(cpControl.errors || {}), mismatch: true });
     } else {
-      if (cpControl.hasError('Las contraseñas no coinciden')) {
+      if (cpControl.hasError('mismatch')) {
         const errors = { ...cpControl.errors };
         delete (errors as any).mismatch;
         if (Object.keys(errors).length === 0) cpControl.setErrors(null);
